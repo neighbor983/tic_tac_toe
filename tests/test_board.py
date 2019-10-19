c98726cf115c12147c336a77aafb650f289030ae
neighbor983 <kenneth.ray.baker@gmail.com>
2019-10-19 12:46:38 -0500

    test for diagonal winner test

--- tests/test_board.py
+++ tests/test_board.py
@@ -9,8 +9,43 @@ def test_reset():
     assert actual == expected, 'reset should create a new board'
 
 
-def test_row_winner():
+def test_row_winner_true():
     b1 = Board(['X', 'X', 'X', '-', '-', '-', '-', '-', '-'])
     actual = b1.isThereAWinnerFromRows()
     expected = True
     assert actual == expected, 'should return true when there is a win in a row'
+
+
+def test_row_winner_false():
+    b1 = Board(['X', '-', 'X', '-', '-', '-', '-', '-', '-'])
+    actual = b1.isThereAWinnerFromRows()
+    expected = False
+    assert actual == expected, 'should return false when there is not a win in a row'
+
+
+def test_col_winner_true():
+    b1 = Board(['X', '-', '-', 'X', '-', '-', 'X', '-', '-'])
+    actual = b1.isThereAWinnerFromCols()
+    expected = True
+    assert actual == expected, 'should return true when there is a win in a col'
+
+
+def test_row_winner_false():
+    b1 = Board(['X', '-', 'X', '-', '-', '-', '-', '-', '-'])
+    actual = b1.isThereAWinnerFromCols()
+    expected = False
+    assert actual == expected, 'should return false when there is not a win in a col'
+
+
+def test_diagonal_winner_true():
+    b1 = Board(['X', '-', '-', '-', 'X', '-', '-', '-', 'X'])
+    actual = b1.isThereAWinnerFromDiagonals()
+    expected = True
+    assert actual == expected, 'should return true when there is a win in a diagonal'
+
+
+def test_diagonal_winner_false():
+    b1 = Board(['X', '-', 'X', '-', '-', '-', '-', '-', '-'])
+    actual = b1.isThereAWinnerFromDiagonals()
+    expected = False
+    assert actual == expected, 'should return false when there is not win in a diagonal'
